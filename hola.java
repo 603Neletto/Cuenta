e6e048736ab72f7b11afa055f40fb92bf672a82f
603Neletto <nelsonkamsy@gmail.com>
2025-02-04 13:38:36 +0000

    Segundo commit: Agregado comentario en hola.java

--- hola.java
+++ hola.java
@@ -1,4 +1,6 @@
-public class hola {
-    
+public class Hola {
+    public static void main(String[] args) {
+        // Este es un mensaje de saludo en Java
+        System.out.println("Hola, mundo!");
+    }
 }
-
