c86ccdefa48eef10c5be7eeb946f3d73b837f6e0
603Neletto <nelsonkamsy@gmail.com>
2025-02-12 08:21:09 +0000

    Cuarto  commit: Agregado archivo K3.java

---
+++ K3.java
@@ -0,0 +1,9 @@
+public class K3 {
+    public static void main(String[] args) {
+        // Este es un mensaje de saludo en Java
+     int var1 = 10;
+     int var2 = 20;
+     int var3 = var1 + var2;
+     System.out.println(var3);
+    }
+}
