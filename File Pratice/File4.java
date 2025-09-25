5a68c45e4a9efafb5f26604be02d5e7c11c59e81
Neletto <nelsonkamsy@gmail.com>
2025-09-25 13:47:16 +0100

    Corregido lo de la carpeta

--- File Pratice/File4.java
+++ File Pratice/File4.java
@@ -1,7 +1,6 @@
+import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.BufferedReader;
-import javax.xml.stream.FactoryConfigurationError;
 //Realiza un programa que lea un fichero "texto.txt" línea a línea con BufferedReader y
 //cuente cuántas líneas contiene, mostrando el resultado en consola. Utiliza manejo de
 //excepciones y cierre seguro del buffer.
@@ -9,9 +8,14 @@ import javax.xml.stream.FactoryConfigurationError;
 public class File4{
     public static void main (String [] args){
         try 
-          (BufferedReader reader= new BuffereReader(new FileReader ("texto.txt"))){
-            
-
+          (BufferedReader reader= new BufferedReader(new FileReader ("Texto.txt"))){
+            String linea;
+            int cuentoLinea= 0;
+            while ((linea = reader.readLine()) != null) {
+                cuentoLinea++;
+            }
+            System.out.println("El archivo contiene " + cuentoLinea+ " lineas.");
+          
                } catch (IOException e) {
             System.out.println("An error occurred: " + e.getMessage());
         }
